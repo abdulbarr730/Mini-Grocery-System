2334838ef13be33bb9154600e49bd988c4315415
Abdul Barr <abdulbarr730@gmail.com>
2026-02-01 22:13:19 +0530

    Models and Database context with seeding

--- GroceryAPI/Models/Order.cs
+++
@@ -1,8 +0,0 @@
-namespace GroceryAPI.Models;
-public class Order {
-    public int Id { get; set; }
-    public int ProductId { get; set; }
-    public int Quantity { get; set; }
-    public decimal TotalPrice { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
